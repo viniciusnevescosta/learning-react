a136d6861af965f979277ef881c639de662d3045
Jolonte <vinicius.nevesco@gmail.com>
2023-12-21 14:36:31 -0300

    refactor: Strict render

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const ROOT_ID = document.getElementById("root");
+
+if (ROOT_ID) {
+  ReactDOM.createRoot(ROOT_ID).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.log("The root element was not find.");
+}
