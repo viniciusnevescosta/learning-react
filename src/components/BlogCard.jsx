bc75ba2041a9672958237d5bdefa87e1f569b4c5
Jolonte <vinicius.nevesco@gmail.com>
2024-01-02 20:26:14 -0300

    :sparkles: feat: BlogCard component

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,21 @@
+const BlogCard = () => {
+  return (
+    <div className="flex flex-col gap-4 p-5 rounded-lg bg-zinc-50 shadow-md">
+      <div className="flex justify-between">
+        <p>02/01/2024</p>
+        <i className="ri-heart-line text-xl"></i>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-3xl">Título</p>
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique
+          senectus et netus et. Risus at ultrices mi tempus imperdiet nulla
+          malesuada pellentesque elit.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+// import { useState } from "react";
+import BlogCard from "../components/BlogCard";
+
 const Home = () => {
   return (
-    <section className="flex-1 bg-sky-100">
-      <h1 className="text-3xl">Latest blogs</h1>
+    <section className="flex-1 gap-5 flex flex-col">
+      <div className="flex justify-between">
+        <h1 className="text-3xl">Latest blogs</h1>
+        <button className="w-fit py-4 px-12 rounded-lg bg-zinc-50">
+          New post
+        </button>
+      </div>
+      <div className="flex-1 flex flex-col gap-2">
+        <BlogCard />
+      </div>
     </section>
   );
 };
