7fbad1efc574620f60f1bc0ff2919ea6cb185d9f
Jolonte <vinicius.nevesco@gmail.com>
2024-01-08 20:19:14 -0300

    feat: Outputting lists

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,36 @@
-// import { useState } from "react";
+import { useState } from "react";
 import BlogCard from "../components/BlogCard";
 
 const Home = () => {
+  const [blogs] = useState([
+    // definir , setBlogs depois
+    {
+      id: 0,
+      author: "vini",
+      date: "02/01/2024",
+      liked: false,
+      title: "teste",
+      body: "oi",
+    },
+    {
+      id: 1,
+      author: "vini",
+      date: "02/01/2024",
+      liked: false,
+      title: "teste",
+      body: "oi",
+    },
+  ]);
+
   return (
     <section className="flex-1 gap-5 flex flex-col">
       <div className="flex justify-between">
         <h1 className="text-3xl">Latest blogs</h1>
-        <button className="w-fit py-4 px-12 rounded-lg bg-zinc-50">
-          New post
-        </button>
       </div>
-      <div className="flex-1 flex flex-col gap-2">
-        <BlogCard />
+      <div className="flex-1 flex flex-col gap-4">
+        {blogs.map((blog) => (
+          <BlogCard key={blog.id} />
+        ))}
       </div>
     </section>
   );
