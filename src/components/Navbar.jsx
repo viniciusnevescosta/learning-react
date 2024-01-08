90ded5dcf31d7ab49b1104d426f6e31d3c1961a5
Jolonte <vinicius.nevesco@gmail.com>
2024-01-08 20:19:32 -0300

    feat: Outputting lists

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      title: "Home",
+      link: "/",
+    },
+    {
+      id: 2,
+      title: "New blog",
+      link: "/",
+    },
+    {
+      id: 3,
+      title: "My account",
+      link: "/",
+    },
+  ]);
+
   return (
     <nav>
       {/* links */}
       <div className="flex p-4 justify-center gap-4 text-xl border-b">
-        <a href="/">Home</a>
-        <a href="/create">New blog</a>
-        <a href="/account">My account</a>
+        {items.map((item) => (
+          <a
+            key={item.key}
+            href={item.link}
+            className="bg-zinc-100 px-5 py-2 rounded-xl"
+          >
+            {item.title}
+          </a>
+        ))}
       </div>
     </nav>
   );
